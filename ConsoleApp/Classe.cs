9e06733783aec64dcef63479e45ad969821fa81f
DAMIEN GASCHY <damien.gaschy@viacesi.fr>
2023-03-07 13:44:13 +0100

    t

---
+++ ConsoleApp/Classe.cs
@@ -0,0 +1,21 @@
+namespace ConsoleApp.Models
+{
+
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Old { get; set; }
+
+        public double AnnualSalaryBrut { get; set; }
+
+        public double Tax { get; set; }
+    }
+
+    
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using ConsoleApp.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User user = new User();
+        user.Id = 1;
+        user.Name = "Damien";
+        user.LastName = "Gaschy";
+        user.Old = 20;
+        user.AnnualSalaryBrut = 6000;
+        user.Tax = 0.2;
+
+        Console.WriteLine($"Id: " +
+            $"{user.Id}, " +
+            $"\nName: {user.Name}, " +
+            $"\nLast Name: {user.LastName}, " +
+            $"\nAge: {user.Old}, " +
+            $"\nAnnual Salary: {user.AnnualSalaryBrut}, " +
+            $"\nTax: {user.Tax}");
+    }
+}
